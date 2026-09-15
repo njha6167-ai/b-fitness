f493eeaadc9744d63de0e29c5bf67c23e4e3beb4
njha6167-ai <njha6167@gmail.com>
2026-09-15 15:10:37 +0530

    Update client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "/api" });
+const api = axios.create({ baseURL: "https://b-fitness.onrender.com/api" });
 
 // ---- Members ----
 export const getMembers = (search = "") =>
